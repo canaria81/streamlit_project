b4d58202e924181eaa8af01d100d0118db150a53
canaria81 <120348849+canaria81@users.noreply.github.com>
2022-12-23 17:06:37 +0900

    화면

---
+++ app_Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def run_home_app():
+    st.text('안녕하세요. 환영합니다')
+    st.text('이 앱은 대한민국 공직자들의 공무원 연금 가입에 대한 통계 앱입니다.') 
+   
+if  __name__=='__main__':
+    main()     
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+from tkinter import Menu
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sb
 from dateutil.parser import parse
 from datetime import date
 
-def main():
-   st.title('공무원연금 가입자 주요통계')
+from app_Home import run_home_app
+from app_eda import run_eda_app
+from app_ml import run_ml_app
 
+def main():
+   st.title('공무원연금 가입자 주요 통계 앱')
+   menu = ['Home', '데이터 분석', '예측']
+   choice = st.sidebar.selectbox('메뉴', menu)
+   img_url = 'https://image.lawtimes.co.kr/images/55555(4).jpg'
+   st.image(img_url)
+   if choice == 'Home':
+       run_home_app()
+   elif choice == '데이터 분석' :
+        run_eda_app()
+   elif choice == '예측' :
+        run_ml_app()  
 if  __name__=='__main__':
-    main()
+    main()   
