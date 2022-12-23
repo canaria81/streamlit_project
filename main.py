75930d183f094b7e74f847f2203eb31b5bd58a71
canaria81 <120348849+canaria81@users.noreply.github.com>
2022-12-23 17:06:20 +0900

    화면

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sb
+from dateutil.parser import parse
+from datetime import date
+
+def main():
+   st.title('공무원연금 가입자 주요통계')
+
+if  __name__=='__main__':
+    main()
