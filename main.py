7755b4656a4d88c97de5f2cf25e08bb83c01e8b0
canaria81 <120348849+canaria81@users.noreply.github.com>
2022-12-25 07:44:05 +0900

    조회 화면 추가

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ from dateutil.parser import parse
 from datetime import date
 
 from app_Home import run_home_app
+from app_search import run_search_app
 from app_eda import run_eda_app
 from app_ml import run_ml_app
 
 def main():
    st.title('공무원연금 가입자 주요 통계 앱')
-   menu = ['Home', '데이터 분석', '예측']
+   menu = ['Home','조회', '데이터 분석', '예측']
    choice = st.sidebar.selectbox('메뉴', menu)
-   img_url = 'https://image.lawtimes.co.kr/images/55555(4).jpg'
-   st.image(img_url)
+   #img_url = 'https://image.lawtimes.co.kr/images/55555(4).jpg'
+   #st.image(img_url)
+   
    if choice == 'Home':
        run_home_app()
+   elif choice == '조회' :
+        run_search_app() 
    elif choice == '데이터 분석' :
         run_eda_app()
    elif choice == '예측' :
         run_ml_app()  
+          
+    
 if  __name__=='__main__':
     main()   
