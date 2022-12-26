55e16b47f4d2258ca5e1c929e516a6ed12e871b2
canaria81 <120348849+canaria81@users.noreply.github.com>
2022-12-26 13:38:01 +0900

    수정

--- app_ml.py
+++ app_ml.py
@@ -8,13 +8,11 @@ import altair as alt
 
 def run_plotly_app():
     df = pd.read_csv('data/공무원연금공단_공무원연금주요통계(지역별 가입자 추이)_20211231.csv', encoding='MS949')
-
-    st.dataframe(df.head())
-
+    st.subheader('전체 공무원 연금 가입건 수  (최근연도 순)')
+    st.dataframe(df.sort_values('구분',ascending= False))
+    st.write(f'전체 데이터  :  {df.shape[0]}행 {df.shape[1]}열 ')
     column_menu = df.columns[ 1 : ]
-
     choice_list = st.multiselect('지역을 선택하세요.', column_menu)
-
     if len(choice_list) != 0 :
         # 유저가 선택한 언어만, 차트를 그린다.
         df_selected = df[choice_list]
@@ -25,11 +23,7 @@ def run_plotly_app():
         ## 스트림릿에서 제공하는 바차트
         st.bar_chart(df_selected)
     df2 = pd.read_csv('data/공무원연금공단_공무원연금주요통계(지역별 가입자 추이)_20211231.csv', encoding='MS949')
-    ### altair 라이브러리의 mark_cicle 함수 사용법
-    chart = alt.Chart(df2).mark_circle().encode(
-        x='petal_length',
-        y='petal_width',
-        color = 'species')
-    st.altair_chart(chart)
+    st.subheader('연금 가입건 수 데이터 5건 ')
+    st.dataframe(df.head())
 if __name__ == '__main__' :
     main()    
